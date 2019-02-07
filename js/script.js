a71f6093331e82ac568c856783fcdf5d1a25d881
IflaMushtaq <nahamiiflamushtaq@gmail.com>
2019-02-07 14:08:27 +0530

    second

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*let bars=document.getElementsByClassName("bar");
+let modal=document.getElementsByClassName("modal");
+
+function mouse(number){ 
+   console.log(bars.length);
+   bars[number].style.visibility="hidden";
+   modal[1].style.display="block";
+   console.log(bars.length);
+}
+
+function proceed(){
+   modal[0].style.display="none"; 
+}*/
+
+$(document).ready(function(){
+    alert("hello");
+});
